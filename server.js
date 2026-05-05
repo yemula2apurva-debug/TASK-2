424d84b72b5b4c94030535469e741d64e6b18ac5
yemula2apurva-debug <yemula2apurva@gmail.com>
2026-05-05 21:08:06 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const connectDB = require('./config/db');
-
-const app = express();
-connectDB();
-
-app.use('/api/files', require('./routes/fileRoutes'));
-
-app.listen(5000, () => console.log('Server running on port 5000'));
