75a78f77b5a6937f55e5567a2d2d0f323c2775db
yemula2apurva-debug <yemula2apurva@gmail.com>
2026-05-05 21:07:22 +0530

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-  await mongoose.connect('mongodb://localhost:27017/fileDB');
-  console.log('MongoDB Connected');
-};
-
-module.exports = connectDB;
